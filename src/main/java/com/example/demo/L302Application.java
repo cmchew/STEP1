1e88961c66848e3b5ff28d352c09143bc3254a43
cmchew <write_on@hotmail.com>
2020-01-08 16:32:17 -0500

    L302

---
+++ src/main/java/com/example/demo/L302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L302Application.class, args);
+    }
+
+}
